941dd828bbf800f63a41ae0831eafe4f3062d695
Krasivaya <semwagacarine@gmail.com>
2020-09-12 14:22:58 +0200

    Add a header component

--- src/Header.js
+++ src/Header.js
@@ -18,9 +18,15 @@ function Header() {
           <span className="header__optionLineTwo">Sign In</span>
         </div>
 
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
 
-        <div className="header__option"></div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
       </div>
     </div>
   );
