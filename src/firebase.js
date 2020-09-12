2e433d6dc3a90de0f326f0d40162c8c8214b0551
Krasivaya <semwagacarine@gmail.com>
2020-09-12 14:13:35 +0200

    Add a header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5HBH77MOZJWcj9Mk45F0TjWUFmp-8uag",
+    authDomain: "clone-5fba8.firebaseapp.com",
+    databaseURL: "https://clone-5fba8.firebaseio.com",
+    projectId: "clone-5fba8",
+    storageBucket: "clone-5fba8.appspot.com",
+    messagingSenderId: "544385974934",
+    appId: "1:544385974934:web:3df051b51c24d37b29b8b7",
+    measurementId: "G-WNM7JGCSZ3"
+};
