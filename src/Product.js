dfff34c48b911397f51af43b50380b3228d539ab
Krasivaya <semwagacarine@gmail.com>
2020-09-18 00:03:17 +0200

    Update body component

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Product() {
-  return <div className="product"></div>;
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
