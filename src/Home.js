c987cc809a32d52c79e365d779feecaa039c9b48
Krasivaya <semwagacarine@gmail.com>
2020-09-18 00:36:23 +0200

    Update body component

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,10 @@ function Home() {
         <Product />
         <Product />
       </div>
-      <div className="home__row">{/* */}</div>
+      <div className="home__row">
+        {" "}
+        <Product />
+      </div>
     </div>
   );
 }
