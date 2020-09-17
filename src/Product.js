3487ebc6a371a2eca60dc797e79a1c9c876b8c7b
Krasivaya <semwagacarine@gmail.com>
2020-09-18 01:02:04 +0200

    Update body component

--- src/Product.js
+++ src/Product.js
@@ -14,7 +14,7 @@ function Product({ title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>⭐</p>;
+              <p>⭐</p>
             ))}
         </div>
       </div>
