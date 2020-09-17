3e248164ab45e2fe7272e15dd0047b1e0c34235b
Krasivaya <semwagacarine@gmail.com>
2020-09-18 00:49:38 +0200

    Update body component

--- src/Product.js
+++ src/Product.js
@@ -5,7 +5,7 @@ function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
           <strong>19.99</strong>
