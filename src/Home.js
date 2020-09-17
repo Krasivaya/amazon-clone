a41b75c8b40b1fdbab2ecd5f55235ace0c13851d
Krasivaya <semwagacarine@gmail.com>
2020-09-18 01:14:48 +0200

    Update body component

--- src/Home.js
+++ src/Home.js
@@ -28,9 +28,27 @@ function Home() {
           />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="56788912"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="23567812"
+            title="Amazon Echo (3rd generaton) | Smart speaker with Alexa, Charcoal Fabric"
+            price={20.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+          />
+          <Product
+            id="10459203"
+            title="Kenwood kMix Stand Mixer for Bakking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={20.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+          />
         </div>
         <div className="home__row">
           <Product />
