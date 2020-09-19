7de72725785dc7063cf6c03438f7992978788e87
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:42:14 +0200

    Update body component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route} 
 from "react-router-dom"
-import Checkout from ".";
+import Checkout from "./Checkout";
 
 function App() {
   return (
