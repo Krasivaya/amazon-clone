b603e71b0f610ea24894033e7d949f8eb7c6fcb0
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:24:47 +0200

    Update body component

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,7 @@ import "./Checkout.css"
 function Checkout() {
     return (
         <div className="checkout">
-            <div className="checkout__img">
+            <div className="checkout__left">
                 <img className="checkout__ad" alt="ad" />
             </div>
         </div>
