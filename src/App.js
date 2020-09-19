776e130f6f256502f9b248d2372da1aabd335696
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:01:27 +0200

    Update body component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router}
+import { BrowserRouter as Router, Switch}
 
 function App() {
   return (
