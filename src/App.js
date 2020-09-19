d70b23d4d642b607a2468da929b965e0a7fae14d
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:20:32 +0200

    Update body component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route} 
 from "react-router-dom"
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route path="/checkout">
             <Header />
-            <Checkout />
+            <Checkoutut />
           </Route>
           <Route path="/">
             <Header />
