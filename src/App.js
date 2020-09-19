aeed5919dc6a3c66c75c5af310925117cf515776
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:06:52 +0200

    Update body component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Home />
           </Route>
           <Route path="/checkout">
-            h2
+            <h1></h1>
           </Route>
         </switch>
     </div>
