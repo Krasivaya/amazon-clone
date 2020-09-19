f2fdc99e45eccd7581c74de424da7cfb4f5cd12f
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:44:19 +0200

    Update body component

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,18 +2,18 @@ import React from 'react'
 import "./Checkout.css"
 
 function Checkout() {
-    return (
-        <div className="checkout">
-            <div className="checkout__left">
-                <img 
-                    className="checkout__ad" 
-                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_.jpg"
-                    alt="ad" 
-                />
-                <h1>gjor</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_.jpg"
+          alt="ad"
+        />
+        <h1>gjor</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Checkout;
