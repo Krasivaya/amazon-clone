f43a85b2dd350e00fa14d55fc40b9fb2c8f4ed43
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:44:04 +0200

    Update body component

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,9 +6,10 @@ function Checkout() {
         <div className="checkout">
             <div className="checkout__left">
                 <img 
-                className="checkout__ad" 
-                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_.jpg"
-                alt="ad" />
+                    className="checkout__ad" 
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1.CB423492668_.jpg"
+                    alt="ad" 
+                />
                 <h1>gjor</h1>
             </div>
         </div>
