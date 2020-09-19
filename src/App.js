7ed96fd855dcb9f8795d28534055f001e5340425
Krasivaya <semwagacarine@gmail.com>
2020-09-19 22:01:11 +0200

    Update body component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserR}
+import { BrowserRouter, }
 
 function App() {
   return (
