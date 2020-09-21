90df993e0de72b67f88f911aee1b9e69274917c0
Krasivaya <semwagacarine@gmail.com>
2020-09-21 22:07:28 +0200

    Update body component

--- src/Header.js
+++ src/Header.js
@@ -34,6 +34,8 @@ function Header() {
           <span className="header__optionLineTwo">Primes</span>
         </div>
 
+        <Link>
+        </Link>
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
           <span className="header__optionLineTwo header__basketCount">0</span>
