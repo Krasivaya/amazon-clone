489ab465d10946cd858da3f1ec595a15c89bebc1
Krasivaya <semwagacarine@gmail.com>
2020-09-21 22:01:10 +0200

    Update body component

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,7 +10,9 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt="ad"
         />
-        
+        <div>
+          
+        </div>
         <h2 className="checkout__title">Your shopping Basket</h2>
       </div>
       <div className="checkout__right">
