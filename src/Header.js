1093e944033406ce5189f3b1bf76e57906f76948
Krasivaya <semwagacarine@gmail.com>
2020-09-21 22:06:55 +0200

    Update body component

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
-      <Link>
+      <Link to=>
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
