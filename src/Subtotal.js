1dc68b1121cdee5259680e0acfc2ba282267d7a5
Krasivaya <semwagacarine@gmail.com>
2020-09-22 12:44:34 +0200

    Update body component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -8,7 +8,7 @@ function Subtotal() {
             <CurrencyFormat 
                 renderText={(value) => (
                     <>
-                    
+                        
                     </>
                 )}
             />
