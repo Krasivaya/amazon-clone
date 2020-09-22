6108f4e6ec9e4b66d2efc1dc1eb109ed5e52cd5f
Krasivaya <semwagacarine@gmail.com>
2020-09-22 12:45:15 +0200

    Update body component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -9,7 +9,7 @@ function Subtotal() {
                 renderText={(value) => (
                     <>
                         <p>
-                          Sutotal (0 items)
+                          Sutotal (0 items): <strong
                         </p>
                     </>
                 )}
