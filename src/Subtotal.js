432588e520be0778269566fee6a00261b6fed85d
Krasivaya <semwagacarine@gmail.com>
2020-09-22 12:46:11 +0200

    Update body component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -12,7 +12,7 @@ function Subtotal() {
               Sutotal (0 items): <strong>0</strong>
             </p>
             <small className="subtotal__gift">
-              <input type="checkout"
+              <input type="checkout" />
             </small>
           </>
         )}
