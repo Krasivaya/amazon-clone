527f7e5a2de631b66f1182829d374127dbfbeb0e
Krasivaya <semwagacarine@gmail.com>
2020-09-25 18:02:18 +0200

    Update data layer

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Link } from 'react-route-dom'
+import { Link } from 'react-router-dom'
 
 function Login() {
     return (
         <div className="login">
             <Link to="/">
-                <img 
-                className="login__logo" 
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt="Amazon logo" 
+                <img
+                    className="login__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt="Amazon logo"
                 />
             </Link>
+
+            <div className="login__container">
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5></h5>
+                </form>
+            </div>
         </div>
     )
 }
