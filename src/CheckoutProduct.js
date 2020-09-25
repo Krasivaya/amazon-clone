bee745114e7cbef94dcb6e3422c03e161d9f805a
Krasivaya <semwagacarine@gmail.com>
2020-09-25 15:47:12 +0200

    Update data layer

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,1 +1,16 @@
-r
+import React from 'react'
+import './CheckoutProduct.css';
+
+function CheckoutProduct({id, image, title, price, rating}) {
+    return (
+        <div className="checkoutProduct">
+            <img className="checkoutPtoduct__image" src={image} />
+
+            <div className="checkoutProduct__info">
+                <p className="CheckoutProduct"
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
