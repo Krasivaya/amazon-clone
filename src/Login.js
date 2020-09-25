d186d89f7bd21f64c0dcd9e9284df305030e6081
Krasivaya <semwagacarine@gmail.com>
2020-09-25 17:52:13 +0200

    Update data layer

---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="" alt="Amazon logo" />
+        </div>
+    )
+}
+
+export default Login
