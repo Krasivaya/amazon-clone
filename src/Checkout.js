3442645b1b400086508ff96e42ca54dab491e518
Krasivaya <semwagacarine@gmail.com>
2020-09-25 16:07:12 +0200

    Update data layer

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Checkout.css"
 import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 
 function Checkout() {
   const [{basket}, dispatch] = useStateValue();
@@ -22,7 +23,7 @@ function Checkout() {
               title={item.title}
               image={item.image}
               price={item.price}
-              id={item.id}
+              rating={item.rating}
             />
           ))}
         </div>
