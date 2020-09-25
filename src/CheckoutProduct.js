b0812530de7649ea1ab7941abb43d94ed43153d6
Krasivaya <semwagacarine@gmail.com>
2020-09-25 15:57:12 +0200

    Update data layer

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import './CheckoutProduct.css';
 
-function CheckoutProduct({id, image, title, price, rating}) {
-    return (
-        <div className="checkoutProduct">
-            <img className="checkoutPtoduct__image" src={image} alt="Checkout Product" />
+function CheckoutProduct({ id, image, title, price, rating }) {
+	return (
+		<div className="checkoutProduct">
+			<img className="checkoutPtoduct__image" src={image} alt="Checkout Product" />
 
-            <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title"></p>
-                <p className="checkoutProduct__price">
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className="checkoutProduct__rating">
-                    
-                </div>
-            </div>
-        </div>
-    )
+			<div className="checkoutProduct__info">
+				<p className="checkoutProduct__title"></p>
+				<p className="checkoutProduct__price">
+					<small>$</small>
+					<strong>{price}</strong>
+				</p>
+				<div className="checkoutProduct__rating">
+					{Array(rating)
+						.fill()
+						.map((_, i) => (
+							<p><span role="img" aria-label="star">⭐</span></p>
+						))}
+				</div>
+				<button>Remove from Basket</button>
+			</div>
+		</div>
+	)
 }
 
 export default CheckoutProduct
