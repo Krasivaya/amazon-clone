d1509adefa15ff547a8a4be228190332791f6624
Krasivaya <semwagacarine@gmail.com>
2020-09-25 13:52:10 +0200

    Update data layer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,13 @@
 export const initialState = {
     basket: [],
 }
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'ADD_TO_BASKET':
+            return {
+							...state,
+							basket: [...sta]
+						}
+    }
+}
