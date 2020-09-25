7c286af8f628c5f9e015943c30f17fae200527f9
Krasivaya <semwagacarine@gmail.com>
2020-09-25 13:35:32 +0200

    Update checkout component

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,9 +1,11 @@
-const { createContext, useContext, useReducer } = require("react")
-
 import React, { createContext, useContext, useReducer} from "react";
 
-import const StateContext = createContext();
+export const StateContext = createContext();
 
 export const StateProvider = ({ reducer, initialState, children }) => (
-    <StateContext.Provider value={useReducer(reducer, initialState)}></StateContext.Provider>
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
 )
+
+export const useStateValue = () => useContext(StateContext);
