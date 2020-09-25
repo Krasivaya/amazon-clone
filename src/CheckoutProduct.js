ccea47098e04b2efdb17c12873457a22be5fc6a9
Krasivaya <semwagacarine@gmail.com>
2020-09-25 15:52:12 +0200

    Update data layer

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -4,10 +4,17 @@ import './CheckoutProduct.css';
 function CheckoutProduct({id, image, title, price, rating}) {
     return (
         <div className="checkoutProduct">
-            <img className="checkoutPtoduct__image" src={image} />
+            <img className="checkoutPtoduct__image" src={image} alt="Checkout Product" />
 
             <div className="checkoutProduct__info">
-                <p className="CheckoutProduct"
+                <p className="checkoutProduct__title"></p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    
+                </div>
             </div>
         </div>
     )
