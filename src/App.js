ed63d29739112642d0959e617915b2c8da44d1b5
Krasivaya <semwagacarine@gmail.com>
2020-09-25 17:52:13 +0200

    Update data layer

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route} 
 from "react-router-dom"
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route path="/login">
-            <h1>Login page</h1>
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
