b2fbbbf652101efc20f201aaa465bf7c014970b9
Krasivaya <semwagacarine@gmail.com>
2020-09-25 14:17:10 +0200

    Update data layer

--- src/Product.js
+++ src/Product.js
@@ -5,9 +5,18 @@ function Product({ id, title, image, price, rating }) {
   const [state, dispatch] = useStateValue();
 
   const addToBasket = () => {
-    
-  }
-
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+  
   return (
     <div className="product">
       <div className="product__info">
