021411e8ed369538f8cffc3676e7675d08bc92ce
Krasivaya <semwagacarine@gmail.com>
2020-09-27 19:12:45 +0200

    -Add payment basket items length

--- src/Payment.js
+++ src/Payment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
+import { Link } from 'react-router-dom';
 
 function Payment() {
   const [{user, basket}, dispatch] = useStateValue();
@@ -8,6 +9,9 @@ function Payment() {
   return (
     <div className='payment'>
       <div className='payment__container'>
+        <h1>
+          Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+        </h1>
         <div className='payment__section'>
           <div className='payment__title'>
             <h3>Delivery Address</h3>
@@ -36,7 +40,14 @@ function Payment() {
           </div>
         </div>
 
-        <div className='payment__section'></div>
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Payment Method</h3>
+          </div>
+          <div className='payment__details'>
+            
+          </div>
+        </div>
       </div>
       
     </div>
