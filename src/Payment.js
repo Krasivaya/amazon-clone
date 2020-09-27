5522083acea7e3709535559d1bb3b15d96f91aea
Krasivaya <semwagacarine@gmail.com>
2020-09-27 19:01:32 +0200

    -Add payment address

--- src/Payment.js
+++ src/Payment.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useStateValue } from './StateProvider';
 
 function Payment() {
+  const [{user}, dispatch] = useStateValue();
+
   return (
     <div className='payment'>
       <div className='payment__container'>
         <div className='payment__section'>
           <div className='payment__title'>
             <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
           </div>
         </div>
 
