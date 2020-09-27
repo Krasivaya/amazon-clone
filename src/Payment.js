68fa8b8e2a661da9fc79366e0e74b96f138d1985
Krasivaya <semwagacarine@gmail.com>
2020-09-27 19:06:09 +0200

    -Add payment items

--- src/Payment.js
+++ src/Payment.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 function Payment() {
-  const [{user}, dispatch] = useStateValue();
+  const [{user, basket}, dispatch] = useStateValue();
 
   return (
     <div className='payment'>
@@ -18,7 +19,22 @@ function Payment() {
           </div>
         </div>
 
-        <div className='payment__section'></div>
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className='payment__items'>
+            {basket.map(item => (
+              <CheckoutProduct 
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
 
         <div className='payment__section'></div>
       </div>
