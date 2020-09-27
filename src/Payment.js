5681d18ba3bd9c5a1fabd13d9fb0364e9d3b4695
Krasivaya <semwagacarine@gmail.com>
2020-09-27 18:58:36 +0200

    -Add payment title

---
+++ src/Payment.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Payment() {
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+        </div>
+
+        <div className='payment__section'></div>
+
+        <div className='payment__section'></div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Payment
