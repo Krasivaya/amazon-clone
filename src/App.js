393e6c348e1a45b6336fff5950d4ac46ba2d2d04
Krasivaya <semwagacarine@gmail.com>
2020-09-27 20:01:41 +0200

    -Add stripe promise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route} 
-from "react-router-dom"
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
-import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  'pk_test_51HW3xzLkfDTCTqC5xc7WtvYlfbQB2q2GwQqc4dSc0kVDIgphfd8fe2g8BuAsnP63Lu0JK7ODLbTJS0Ic6So5EK0p00jlH9ZwJF'
+  );
 
 function App() {
   // eslint-disable-next-line no-empty-pattern
@@ -47,7 +52,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
