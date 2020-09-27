af7f0252568881519f2862be787f402981828f65
Krasivaya <semwagacarine@gmail.com>
2020-09-27 16:07:19 +0200

    Feat(Auth): User should be able to sign-in
    - Set a user

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 
 function Header() {
-  const [{basket}, dispatch] = useStateValue();
+  // eslint-disable-next-line no-unused-vars
+  const [{basket, user}, dispatch] = useStateValue();
 
   return (
     <div className="header">
@@ -25,7 +26,7 @@ function Header() {
         <Link to='/login'>
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out': 'Sign In'}</span>
           </div>
         </Link>
 
