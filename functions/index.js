aea36b754c93c9d980d62b8481e1585b295b3408
Krasivaya <semwagacarine@gmail.com>
2020-10-14 11:01:05 +0200

    Add components

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,33 @@
 const functions = require('firebase-functions');
+const express = require("express");
+const cors = require("cors");
+const { request, response } = require('express');
+const { scryRenderedComponentsWithType } = require('react-dom/test-utils');
+const stripe = require("stripe")('sk_test_51HW3xzLkfDTCTqC5RwBS7yRWqgc3WfcEBxFLXkmREyBcikxxRVx9M4r08XYOaQeA1zTVIxtekSqERy0j3wnB3oKI00khB6R7hu')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+app.post('/payments/create', async (request, response) => {
+  const total = request.query.total;
+  
+  const paymenIntent = await scryRenderedComponentsWithType.paymenIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymenIntent.client_secret,
+  })
+})
+
+// Listen command
+exports.api = functions.https.onRequest(app)
